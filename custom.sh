16a8ff1617846493d7666f2dba3bd0757f65c8a0
bmw1231 <113307927+bmw1231@users.noreply.github.com>
2024-03-01 18:44:22 +0000

    main

--- custom.sh
+++ custom.sh
@@ -1,10 +1,3 @@
-firefox_search.yml:
-
-Skript:
-  - Name: Firefox-Suche nach "rogger rogger"
-    Schritte:
-      - Schritt: "Firefox öffnen"
-        Aktion: "firefox_open"
-      - Schritt: "Suche durchführen"
-        Aktion: "search_query"
-        Suchbegriff: "rogger rogger"
+# config.yml
+open_website:
+  website_url: "https://www.google.com"
